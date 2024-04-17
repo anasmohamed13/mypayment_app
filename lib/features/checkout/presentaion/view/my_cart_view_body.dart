2802770f149520c331e1bee96497d2b1e9cfc473
anas mohamed <anas2003610@gmail.com>
2024-04-18 01:02:20 +0200

    create order info item

--- lib/features/checkout/presentaion/view/my_cart_view_body.dart
+++ lib/features/checkout/presentaion/view/my_cart_view_body.dart
@@ -1,17 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:mypayment_inegration/core/utils/styles.dart';
 
 class MyCartViewBody extends StatelessWidget {
-  const MyCartViewBody({super.key});
+  const MyCartViewBody({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+              'assets/image/—Pngtree—supermarket food green shopping basket_6295482.png'),
+          const SizedBox(
+            height: 25,
+          ),
+          const OrderInfoItem(
+            title: 'Order subtotal',
+            value: r'$39.99',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'Discount',
+            value: r'$0',
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const OrderInfoItem(
+            title: 'shipping',
+            value: r'$8',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderInfoItem extends StatelessWidget {
+  const OrderInfoItem({super.key, required this.title, required this.value});
+  final String title, value;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
       children: [
-        const SizedBox(
-          height: 20,
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
         ),
-        Image.asset(
-            'assets/image/—Pngtree—supermarket food green shopping basket_6295482.png')
+        const Spacer(),
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
       ],
     );
   }
