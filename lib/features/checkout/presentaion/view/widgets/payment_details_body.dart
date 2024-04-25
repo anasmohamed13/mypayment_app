cf9469824d8cadfac084aef379d2bf92bb9b62a4
anas mohamed <anas2003610@gmail.com>
2024-04-25 20:17:30 +0200

    select payment method & create credit card (part 1)

--- lib/features/checkout/presentaion/view/widgets/payment_details_body.dart
+++ lib/features/checkout/presentaion/view/widgets/payment_details_body.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'payment_method_list_view.dart';
 
 class PaymentDetailsBody extends StatelessWidget {
@@ -11,6 +11,36 @@ class PaymentDetailsBody extends StatelessWidget {
     return const Column(
       children: [
         PaymentMethodsListView(),
+        CustomCreditCard(),
+      ],
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (value) {},
+        )
       ],
     );
   }
--- lib/features/checkout/presentaion/view/widgets/payment_method_list_view.dart
+++ lib/features/checkout/presentaion/view/widgets/payment_method_list_view.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'payment_method_item.dart';
 
-class PaymentMethodsListView extends StatelessWidget {
+class PaymentMethodsListView extends StatefulWidget {
   const PaymentMethodsListView({super.key});
 
+  @override
+  State<PaymentMethodsListView> createState() => _PaymentMethodsListViewState();
+}
+
+class _PaymentMethodsListViewState extends State<PaymentMethodsListView> {
   final List<String> paymentMethodItems = const [
     'assets/image/masterdcard.svg',
     'assets/image/paypal.svg'
   ];
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +26,15 @@ class PaymentMethodsListView extends StatelessWidget {
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: PaymentMethodItem(
-                isActive: false,
-                image: paymentMethodItems[index],
+              child: GestureDetector(
+                onTap: () {
+                  activeIndex = index;
+                  setState(() {});
+                },
+                child: PaymentMethodItem(
+                  isActive: activeIndex == index,
+                  image: paymentMethodItems[index],
+                ),
               ),
             );
           }),
