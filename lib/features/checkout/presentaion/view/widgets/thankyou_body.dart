d94a20e19896a0fb8a0743e59ea4ca7937cd0461
anas mohamed <anas2003610@gmail.com>
2024-04-26 16:00:11 +0300

    custom shaped container part (2)

--- lib/features/checkout/presentaion/view/widgets/thankyou_body.dart
+++ lib/features/checkout/presentaion/view/widgets/thankyou_body.dart
@@ -6,8 +6,9 @@ class ThankYouViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32),
+      padding: const EdgeInsets.all(20),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: ShapeDecoration(
@@ -29,6 +30,23 @@ class ThankYouViewBody extends StatelessWidget {
             bottom: MediaQuery.sizeOf(context).height * .2,
             child: const CircleAvatar(
               backgroundColor: Colors.white,
+            ),
+          ),
+          const Positioned(
+            top: -50,
+            left: 0,
+            right: 0,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color(0xFFD9D9D9),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Color(0xff34A853),
+                child: Icon(
+                  Icons.check,
+                  size: 50,
+                ),
+              ),
             ),
           ),
         ],
