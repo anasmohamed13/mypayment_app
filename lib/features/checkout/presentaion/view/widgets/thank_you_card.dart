1a1b54ee2d694f7b5db8b189e05f7c7ab682bb68
anas mohamed <anas2003610@gmail.com>
2024-04-28 15:10:28 +0300

    create payment item info

--- lib/features/checkout/presentaion/view/widgets/thank_you_card.dart
+++ lib/features/checkout/presentaion/view/widgets/thank_you_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mypayment_inegration/core/utils/styles.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({
@@ -8,12 +9,75 @@ class ThankYouCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: ShapeDecoration(
         color: const Color(0xFFD9D9D9),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
       ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50 + 16, left: 22, right: 22),
+        child: Column(
+          children: [
+            const Text(
+              'Thank You',
+              textAlign: TextAlign.center,
+              style: Styles.style25,
+            ),
+            Text(
+              'You transaction was successful',
+              textAlign: TextAlign.center,
+              style: Styles.style20,
+            ),
+            const SizedBox(
+              height: 42,
+            ),
+            const PaymentItemInfo(
+              title: 'Date',
+              value: '01/10/2024',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'Time',
+              value: '10:30 PM',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const PaymentItemInfo(
+              title: 'To',
+              value: 'Sam Louis',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentItemInfo extends StatelessWidget {
+  const PaymentItemInfo({super.key, required this.title, required this.value});
+  final String title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: Styles.style18,
+        ),
+        Text(
+          value,
+          textAlign: TextAlign.center,
+          style: Styles.styleBold18,
+        ),
+      ],
     );
   }
 }
